972c6100d4aaacf5ad311733bd58515049a605cc
amirh <a_h_berenji@yahoo.com>
2023-03-16 22:36:34 +0330

    including AE class within the .py file

--- single_dashboard.py
+++ single_dashboard.py
@@ -11,9 +11,35 @@ from dash_extensions.enrich import Output, DashProxy, Input, MultiplexerTransfor
 import plotly.graph_objects as go
 
 import joblib
-from assets.AE import AE
+# from assets.AE import AE
 import torch
 from sklearn.metrics import mean_squared_error
+
+import torch
+
+class AE(torch.nn.Module):
+  def __init__(self):
+    super(AE, self).__init__()
+    self.encoder = torch.nn.Sequential(
+            torch.nn.Linear(2048, 1024),
+            torch.nn.Tanh(),
+            torch.nn.Linear(1024, 512),
+            torch.nn.Tanh(),
+            torch.nn.Linear(512, 256),
+            torch.nn.Tanh(),
+        )
+    self.decoder = torch.nn.Sequential(
+            torch.nn.Linear(256, 512),
+            torch.nn.Tanh(),
+            torch.nn.Linear(512, 1024),
+            torch.nn.Tanh(),
+            torch.nn.Linear(1024, 2048),
+            torch.nn.Tanh(),
+        )
+  def forward(self, x):
+          encoded = self.encoder(x)
+          decoded = self.decoder(encoded)
+          return decoded
 
 global f
 f = np.linspace(0, 1000, 2048)
