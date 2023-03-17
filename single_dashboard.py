e484abeba9554d139298861a8b1109817d0022ba
amirh <a_h_berenji@yahoo.com>
2023-03-17 11:49:37 +0330

    better app initiation

--- single_dashboard.py
+++ single_dashboard.py
@@ -61,10 +61,6 @@ def softmax_calculator(input_diction):
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = DashProxy(__name__,transforms=[MultiplexerTransform()], external_stylesheets=external_stylesheets)
-app.css.append_css({
-    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
-})
-
 server = app.server
 
 importing_path = os.getcwd() + '/assets/'
